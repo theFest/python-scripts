d7c76da4999e9bd25d33a4ac2cecad02b720ba1b
theFest <stjepanb83@gmail.com>
2022-07-05 17:41:04 +0200

    Initial CheckYourMood
    n/a atm

---
+++ CheckYourMood.py
@@ -0,0 +1,14 @@
+mood = input("What is your mood today? ")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Cheer up, mate!")
+elif mood == "excited":
+    print("Woohoo!")
+elif mood == "relaxed":
+    print("Yeah, today is the day to chill...")
+else:
+    print("Unknown mood or feeling!")
